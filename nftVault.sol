8e00a46c6aefb94fd9ca7dc22d34e7b0953cfa3a
Krishang Shah <109511742+kamuikatsurgi@users.noreply.github.com>
2022-11-05 23:10:22 +0530

    Update nftVault.sol

--- nftVault.sol
+++ nftVault.sol
@@ -10,10 +10,20 @@ contract nftVault{
     constructor(address _addr){
         cont_addr = _addr;
     }
+/*
+    function addfunds(address msg_sender ,address token_addr, uint token_amount) payable public{
+        Token token = Token(token_addr);
 
-    function recieve() payable external{
+        token.approve(msg.sender, token_amount);
+
+        token.transferFrom(
+            msg_sender,
+            address(this),
+            token_amount);
     }
+*/
 
 //Addvalue: Done 
 //Check Value
+
 }
